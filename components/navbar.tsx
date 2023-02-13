9676fbb925fc92d5f6abe5e6988bd9ddeb7221be
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2023-02-13 00:24:50 +0000

    fix: navbar responsivity

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,19 +1,22 @@
-import { Heading, Box, HStack, Spacer, Divider } from "@chakra-ui/react";
+import { Heading, Box, Spacer, Divider, Stack } from "@chakra-ui/react";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
     <>
       <Box py={5}>
-        <HStack spacing={12}>
+        <Stack
+          spacing={{ base: 3, lg: 12 }}
+          direction={{ base: "column", lg: "row" }}
+        >
           <Link href="/">
             <Heading as="h1">Michael&apos;s D&D Starter Guide</Heading>
           </Link>
-          <Spacer />
+          <Spacer display={{ base: "none", lg: "unset" }} />
           <Link href="/characters">Characters</Link>
           <Link href="/stats">Stats and Equipment</Link>
           <Link href="/dice">Dice</Link>
-        </HStack>
+        </Stack>
       </Box>
       <Divider />
     </>
